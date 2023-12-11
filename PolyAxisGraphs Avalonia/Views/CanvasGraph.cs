a1bbac77a909d0d2abc3cd4b1b1a7922144be427
Bolomorz <dominikschneider8@gmx.de>
2023-12-11 12:50:00 +0000

    hehe xd

---
+++ PolyAxisGraphs Avalonia/Views/CanvasGraph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using PolyAxisGraphs_Backend;
+
+namespace PolyAxisGraphs_Avalonia.Views
+{
+    internal class CanvasGraph
+    {
+        public PolyAxisGraph pag { get; set; }
+        Canvas canvas { get; set; }
+
+        public CanvasGraph(Canvas _canvas)
+        {
+            canvas = _canvas;
+            pag = new PolyAxisGraph(new Settings(@"..\..\..\Settings.txt"));
+        }
+
+        public void SetLanguage(string lngfile)
+        {
+            pag.SetLanguage(lngfile);
+        }
+
+        public void SetFile(string datafile)
+        {
+            pag.SetFilePath(datafile);
+            pag.ReadData();
+            canvas.Children.Clear();
+        }
+    }
+}
--- PolyAxisGraphs Avalonia/Views/MainView.axaml.cs
+++ PolyAxisGraphs Avalonia/Views/MainView.axaml.cs
@@ -1,11 +1,47 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 
 namespace PolyAxisGraphs_Avalonia.Views;
 
 public partial class MainView : UserControl
 {
+    CanvasGraph cg { get; set; }
+
+    public static FilePickerFileType DataFiles = new("DataFiles") { Patterns = new[] { "*.txt", "*.csv", "*.xlsx" } };
     public MainView()
     {
         InitializeComponent();
+        cg = new CanvasGraph(MainCanvas);
+        LoadControls();
+    }
+
+    public void LoadControls()
+    {
+        TBFile.FontFamily = new Avalonia.Media.FontFamily("Consolas");
+        TBFile.FontSize = 9;
+        TBFile.Text = cg.pag.settings.currentlang.FindElement("tbopenfile");
+        BTOpenFile.FontFamily = new Avalonia.Media.FontFamily("Consolas");
+        BTOpenFile.FontSize = 9;
+        BTOpenFile.Content = cg.pag.settings.currentlang.FindElement("btopenfile");
+    }
+
+    private async void OpenFileButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var toplevel = TopLevel.GetTopLevel(this);
+        var files = await toplevel.StorageProvider.OpenFilePickerAsync(new Avalonia.Platform.Storage.FilePickerOpenOptions
+        {
+            Title = "Open Data File",
+            AllowMultiple = false,
+            FileTypeFilter = new[] { DataFiles },
+            SuggestedStartLocation = await toplevel.StorageProvider.TryGetFolderFromPathAsync(
+                new System.Uri(new System.Uri(System.AppDomain.CurrentDomain.BaseDirectory), cg.pag.settings.initialdirectory))
+        });
+
+        if(files != null)
+        {
+            string file = files[0].Path.AbsolutePath;
+            TBFile.Text = file;
+            cg.SetFile(file);
+        }
     }
 }
