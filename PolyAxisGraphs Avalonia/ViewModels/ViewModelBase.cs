93e1a74f7019c1a8e3a479691e77d109a6d67369
Bolomorz <dominikschneider8@gmx.de>
2023-12-11 11:37:08 +0000

    Add project files.

---
+++ PolyAxisGraphs Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PolyAxisGraphs_Avalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ PolyAxisGraphs Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace PolyAxisGraphs_Avalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
